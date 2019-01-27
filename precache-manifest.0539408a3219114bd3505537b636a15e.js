5a628713d85480e716e8fef2e42f27e20b6c10e0
martin_marinov <martin_n_marinov@mail.bg>
2019-01-27 15:52:16 +0200

    init

---
+++ precache-manifest.0539408a3219114bd3505537b636a15e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5b1468c37a7014cbc81e1b0c7d42ce9",
+    "url": "./static/media/map.c5b1468c.png"
+  },
+  {
+    "revision": "acba81d6b15a1ab84ddcfe5d50988af2",
+    "url": "./static/media/logo-horizontal.acba81d6.png"
+  },
+  {
+    "revision": "9fa137d4eebe51fd014af79acaab4d26",
+    "url": "./static/media/how-we-work.9fa137d4.png"
+  },
+  {
+    "revision": "bc603ac8546f24024ff1c86a357f1094",
+    "url": "./static/media/header.bc603ac8.png"
+  },
+  {
+    "revision": "111d0cf0424633ad8207c4dd80bd02cf",
+    "url": "./static/media/girl_temp.111d0cf0.PNG"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "38268d94ab7615056035",
+    "url": "./static/js/main.38268d94.chunk.js"
+  },
+  {
+    "revision": "5508d87df089ca017f6d",
+    "url": "./static/js/1.5508d87d.chunk.js"
+  },
+  {
+    "revision": "38268d94ab7615056035",
+    "url": "./static/css/main.48eee609.chunk.css"
+  },
+  {
+    "revision": "1673c62a5f23d8e38640a971586dd5fd",
+    "url": "./index.html"
+  }
+];
